80a809574b6919b5fc231df1020db3e9e646b94c
Nhlamuloj <nmahlabana@gmail.com>
2023-04-20 18:22:27 +0200

    updated javascript

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-Window.addEventListener('load',()=>{
+window.addEventListener('load', () =>{
     const form = document.querySelector("#new-task-form");
-    const input = document.querySelector
+    const input = document.querySelector("#new-task-input")
+    const list_el = document.querySelector("task")
+    
+    form.addEventListener('submit',(e) => {
+        e.preventDefault();
+        const task =input.ariaValue;
+        if(!task){
+           alert("Please fill out the task")
+        }else{
+            console.log("Success")
+        }
+
+        
+    })
 })
